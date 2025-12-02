cd3de0ff8255dc209635bce243786f54da6780ba
au555720 <au555720@uni.au.dk>
2025-12-02 16:04:25 +0100

    updated version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aaby",
-    version="0.1.1",
+    version="0.1.2",
     description="Automated AMBER System Builder Workflow",
     author="Amanda D. Stange",
     packages=find_packages(),
